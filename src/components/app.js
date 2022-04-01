cbf2b1fb7a003ab299e514a73e5d9e909a7178c8
keskipper <keskipper@comcast.net>
2022-04-01 13:01:07 -0500

    created recent posts and search bar components and used them in the home component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Logo from './logo';
+import SearchBar from "./search-bar";
+import RecentPosts from "./recent-posts";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <Logo/>
+        <SearchBar />
+        <RecentPosts />
       </div>
     );
   }
