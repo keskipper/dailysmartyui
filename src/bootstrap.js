66f9e8143e9a0343cc27807715481cbcf70d99b2
keskipper <keskipper@comcast.net>
2022-04-02 17:51:55 -0500

    added results route

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,12 +4,14 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 import "./style/main.scss";
 import Home from "./components/home";
+import Results from "./components/results";
 
 function main() {
   ReactDOM.render(
@@ -17,7 +19,7 @@ function main() {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Home}/>
-          <Route path='/results' component={Home} />
+          <Route path='/results' component={Results} />
         </Switch>
       </BrowserRouter>
     </Provider>,
