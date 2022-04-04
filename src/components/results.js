0f34901ffac280831a1fd6ebf8c16c1debb98e85
keskipper <keskipper@comcast.net>
2022-04-04 10:50:06 -0500

    made logo custom sizeable, placed SearchBar into results.js

--- src/components/results.js
+++ src/components/results.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Logo from './logo';
+import SearchBar from './search-bar';
 
 export default class Results extends Component {
     render() {
         return (
-            <div><h1>Results</h1>
-            <Logo />
+            <div>
+            <Logo size={55}/>
+            <SearchBar />
             </div>
         );
     }
