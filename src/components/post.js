8d1ead9a344e279320d65b853c3d15ea5dcd9b71
keskipper <keskipper@comcast.net>
2022-04-04 18:34:48 -0500

    created titles for post links, styled post links

--- src/components/post.js
+++ src/components/post.js
@@ -18,17 +18,40 @@ export default class Post extends Component {
         return topics;
     }
 
+    getNameForPostLink(str) {
+        var n = str.lastIndexOf('/');
+        var res = str.substring(n+1, str.length);
+
+        if((n+1) == str.length) {
+            res = str.slice(0, n);
+            n = res.lastIndexOf('/');
+            res = str.substring(n+1, str.length-1);
+        }
+        
+        if(res.includes('.html')) {
+            res = res.substring(0, res.length -5)
+        }
+        if(res.includes('.htm')) {
+            res = res.substring(0, res.length -4)
+        }
+        
+        return res;
+    }
+
     renderLinks() {
         let links = this.props.post_links.map((post_link, index) => {
             return (
                 <div className="post-link" key={index} >
                     <div className="post-link__box" />
                     <div className="post-link__link">
-                        <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+                        <a href={post_link.link_url}>{this.getNameForPostLink(post_link.link_url)}</a>
                     </div>
                 </div>
             )
         })
+        if(links == 0) {
+            return <div className="no-content">No links to display</div>
+        }
         return links;
     }
 
@@ -63,6 +86,7 @@ export default class Post extends Component {
                         height={this.state.height}
                     >
                         <div className="result-post__links">
+                            
                             {this.renderLinks()}
                         </div>  
                     </AnimateHeight>
